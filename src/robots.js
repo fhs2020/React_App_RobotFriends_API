32545d6ed08e234191a810583dfbda67c937fe22
Flavio Henrique de Sousa <flavio.sousa@lggo.com.br>
2019-09-05 13:57:42 -0300

    First commit

---
+++ src/robots.js
@@ -0,0 +1,22 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Ron',
+		username: 'Bret',
+		email: 'ron@yahoo.com'
+	},
+	{
+				id: 2,
+		name: 'Rada',
+		username: 'Breta',
+		email: 'Rada@yahoo.com'
+	},
+		{
+				id: 3,
+		name: 'Cris',
+		username: 'Duarte',
+		email: 'cris.Duarte@yahoo.com'
+	}
+
+
+]
