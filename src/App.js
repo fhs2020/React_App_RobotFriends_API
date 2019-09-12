b41b7b5e31d3add036c3b7dffb08d9b2cc9704ed
Flavio Henrique de Sousa <flavio.sousa@lggo.com.br>
2019-09-12 13:59:11 -0300

    Adding State

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+ 
 
-const App = () => {
+class App extends Compon
+		super()
+		this.state = {
+			 	robots: robots,
+ 				searchfield: ''	
+		}
+	}
+
+onsearchChange(event){
+	console.log(event);
+}
+
+	render(){
 	return (
 		<div className='tc'>
 			<h1>RobotFriends</h1>
 			<SearchBox />
-			<CardList robots={robots}/>
+			<CardList robots={this.state.robots}/>
 		</div>
-		);
 
+		);
+	}
 }
 
 export default App;
