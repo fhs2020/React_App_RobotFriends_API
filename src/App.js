e4539c1c9f9b3a4dc81b63f4c706acc45721dc1d
Flavio Henrique de Sousa <flavio.sousa@lggo.com.br>
2019-09-20 07:39:32 -0300

    Adding style and sega-logo-font

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
- 
+import './App.css';
 
 class App extends Component{
 	constructor(){
@@ -25,7 +25,7 @@ onSearchChange = (event) => {
 	})
 	return (
 		<div className='tc'>
-			<h1>RobotFriends</h1>
+			<h1 className='f2'>RobotFriends</h1>
 			<SearchBox searchChange={this.onSearchChange} />
 			<CardList robots={filteredRobots}/>
 		</div>
