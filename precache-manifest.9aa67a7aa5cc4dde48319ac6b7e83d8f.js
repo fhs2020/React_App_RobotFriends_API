e5871d68185c1998f40edc13bdf7d2e3f02814e6
Flavio Henrique de Sousa <flavio.sousa@lggo.com.br>
2019-11-07 13:32:21 -0300

    Updates

---
+++ precache-manifest.9aa67a7aa5cc4dde48319ac6b7e83d8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6989660bb99fd532758a24ded34d5308",
+    "url": "/React_App_RobotFriends_API/index.html"
+  },
+  {
+    "revision": "899b2e7bf74a28e823f6",
+    "url": "/React_App_RobotFriends_API/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "946904451ec973556038",
+    "url": "/React_App_RobotFriends_API/static/css/main.bba1d1d9.chunk.css"
+  },
+  {
+    "revision": "899b2e7bf74a28e823f6",
+    "url": "/React_App_RobotFriends_API/static/js/2.666ab0ce.chunk.js"
+  },
+  {
+    "revision": "946904451ec973556038",
+    "url": "/React_App_RobotFriends_API/static/js/main.d18b4ac6.chunk.js"
+  },
+  {
+    "revision": "8571f18bf9bc6c8530fb",
+    "url": "/React_App_RobotFriends_API/static/js/runtime~main.101d5f4c.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/React_App_RobotFriends_API/static/media/SEGA.82e09f78.TTF"
+  }
+]);
